940da16e1091108576cb9faa49682dd0ccdda9b6
JBMjese <6935@holbertonstudents.com>
2024-05-02 14:21:01 -0500

    :art: Scrip to collect all tasks from all employees

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""Script to use a REST API to export data in JSON format"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    API_URL = "https://jsonplaceholder.typicode.com"
+
+    response = requests.get(f"{API_URL}/users")
+    users = response.json()
+
+    all_employees = {}
+
+    for user in users:
+        user_id = user["id"]
+        response = requests.get(
+            f"{API_URL}/users/{user_id}/todos",
+            params={"_expand": "user"}
+        )
+        tasks = response.json()
+
+        user_tasks = []
+        for task in tasks:
+            task_dict = {
+                "username": task["user"]["username"],
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            user_tasks.append(task_dict)
+
+        all_employees[user_id] = user_tasks
+
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(all_employees, file)
