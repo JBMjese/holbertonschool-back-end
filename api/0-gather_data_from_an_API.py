935bd0d92d92c9e55724b7c6b7cce8f223047ff5
JBMjese <6935@holbertonstudents.com>
2024-05-02 11:38:34 -0500

    :adhesive_bandage: fix point 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import requests
 import sys
+
+
 """
 Script to retrieve and display TODO list progress for a given employee ID
 using a REST API.
+
+Usage: python3 script_name.py employee_id
 """
 
 
